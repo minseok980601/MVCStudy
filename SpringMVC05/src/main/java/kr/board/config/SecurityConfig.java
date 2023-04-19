fe2bf8b26dfd25dbec1dab7e6e9a782b7cae3702
minseok980601 <minseok980601@gmail.com>
2023-04-19 22:58:03 +0900

    20230419 MVC05 Study

---
+++ SpringMVC05/src/main/java/kr/board/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package kr.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration		// 환경설정파일
+@EnableWebSecurity	// 웹과 스프링 Security단을 연결시킴
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	// 보안과 관련된 요청에 대한 설정
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+		filter.setEncoding("UTF-8");
+		filter.setForceEncoding(true);
+		http.addFilterBefore(filter, CsrfFilter.class);
+	}
+	
+	// 패스워드 인코디 객체 설정
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+}
---
+++ SpringMVC05/src/main/java/kr/board/mapper/MemberMapper.java
@@ -0,0 +1,22 @@
+package kr.board.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import kr.board.entity.AuthVO;
+import kr.board.entity.Board;
+import kr.board.entity.Member;
+
+@Mapper
+public interface MemberMapper {
+	
+	public Member registerCheck(String memID);
+	public int register(Member m);		// 회원등록(성공1, 실패0)
+	public Member memLogin(Member mvo);	// 로그인 체크
+	public int memUpdate(Member mvo);	// 수정하기
+	public Member getMember(String memID);	// 회원ID 가져오기
+	public void memProfileUpdate(Member mvo);	// 이미지 업로드
+	public void authInsert(AuthVO saveVO);
+}
